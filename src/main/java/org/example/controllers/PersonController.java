8f59741cc825ac2c9b899526326d4fd05aa90156
Belyshev Artem <artem.belyshev@kctdata.cz>
2023-03-07 13:51:39 +0100

    test uni project

---
+++ src/main/java/org/example/controllers/PersonController.java
@@ -0,0 +1,13 @@
+package org.example.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/person")
+public class PersonController {
+}
